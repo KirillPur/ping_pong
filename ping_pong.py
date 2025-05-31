f6467eac1fd2f7612a5b769b1d290435bf3c794e
KirillPur <kirillpurtov68@gmail.com>
2025-05-31 16:45:14 +0500

    v1.1.6 Создание игроков

--- ping_pong.py
+++ ping_pong.py
@@ -1,31 +1,51 @@
 import pygame as pg
-mw = pg.display.set_mode((500, 500))
+WIN_WIDTH = 500
+WIN_HEIGHT = 500
+BLACK = (0,0,0)
+bg = BLACK
+mw = pg.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 clock = pg.time.Clock()
 game = True
 
 class GameSprite(pg.sprite.Sprite):
-    def __init__(self, sprite_image, sprite_x, sprite_y, sprite_speed, width, height):
+    def __init__(self, sprite_image, sprite_x, sprite_y, sprite_speed, width, height, key_up = pg.K_UP, key_down = pg.K_DOWN):
         super().__init__()
-        self.image = transform.scale(image.load(sprite_image), (width, height))
+        self.image = pg.transform.scale(pg.image.load(sprite_image), (width, height))
         self.speed = sprite_speed
         self.rect = self.image.get_rect()
         self.rect.x = sprite_x
         self.rect.y = sprite_y
+        self.key_up = key_up
+        self.key_down = key_down
     def reset(self):
-        window.blit(self.image, (self.rect.x, self.rect.y))
+        mw.blit(self.image, (self.rect.x, self.rect.y))
+    def fill(self):
+        pg.draw.rect(mw, bg, self.rect)
 
 class Player(GameSprite):
     def update(self):
-        keys_pressed = key.get_pressed()
-        if keys_pressed[K_UP] and self.rect.y > SPEED:
-            self.rect.y -= SPEED
-        if keys_pressed[K_DOWN] and self.rect.y < WIN_HEIGHT - SPEED - SPRITE_SIZE:
-            self.rect.y += SPEED
+        keys_pressed = pg.key.get_pressed()
+        if keys_pressed[self.key_up] and self.rect.y > self.speed:
+            self.rect.y -= self.speed
+        if keys_pressed[self.key_down] and self.rect.y < WIN_HEIGHT - self.speed - self.rect.height:
+            self.rect.y += self.speed
+
+player_1 = Player("racket.png",10,100,4,50,150,pg.K_w,pg.K_s)
+player_2 = Player("racket.png",440,200,4,50,150)
 
 while game == True:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             game = False
     
+    player_1.fill()
+    player_2.fill()
+
+    player_1.update()
+    player_2.update()
+
+    player_1.reset()
+    player_2.reset()
+
     pg.display.update()
     clock.tick(60)
